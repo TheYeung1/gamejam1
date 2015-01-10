c0187ad78dd929bed836efd7667ae8fb16575baf
Winston Yeung <yeung.winston@gmail.com>
2015-01-10 12:20:03 -0500

    make box move

---
+++ Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+
+	CharacterController controller;
+	// Use this for initialization
+	void Start () {
+		this.controller = this.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		this.transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal"), 0));
+		controller.Move(this.transform.forward * Input.GetAxis("Vertical"));
+	}
+}
